11833390717eb2570f03b874de3cebfcc14b2681
Developer-felix <onjombafelix1@gmail.com>
2021-03-31 08:22:36 -0700

    final configurations

--- inventory/models.py
+++ inventory/models.py
@@ -17,13 +17,13 @@ class Device(models.Model):
     def __str__(self):
         return 'Type : {0} Price : {1}'.format(self.type,self.price)
 
-class Laptop(Device):
+class Laptops(Device):
     pass
 
-class Desktop(Device):
+class Desktops(Device):
     pass
 
-class Mobile(Device):
+class Mobiles(Device):
     pass
 
     
